f015d68fbce06c5e4c5eec487f53db0a40cf5398
Cole Strain <colestrain2000@gmail.com>
2023-08-15 20:56:42 -0700

    Finished tutorial!

--- src/Scripts/GraffitiTutoiral.cs
+++ src/Scripts/GraffitiTutoiral.cs
@@ -8,14 +8,17 @@ public class GraffitiTutorial : UpdatableAndDeletable
 {
     public enum Phase
     {
-        Init,
         Craft,
         Spray,
         Throw,
         End
     }
-    public Phase currentPhase { get; set; } = Phase.Init;
-    public Vector2 craftTriggerPos { get; } = new(820.0f, 290.0f);
+    public Phase nextPhase { get; set; } = Phase.Craft;
+    public Vector2 playerPos = Vector2.zero;
+    public Vector2 craftTriggerPos { get; } = new(742f, 842f);
+    public Vector2 sprayTriggerPos { get; } = new(190f, 1626f);
+    public Vector2 throwTriggerPos { get; } = new(742f, 2442f);
+    public float triggerReach = 100f;
 
     public GraffitiTutorial(Room room)
     {
@@ -26,20 +29,46 @@ public class GraffitiTutorial : UpdatableAndDeletable
     {
         base.Update(eu);
 
-        if (!room.fullyLoaded || !room.BeingViewed || currentPhase == Phase.End) return;
+        if (!room.fullyLoaded || !room.BeingViewed || nextPhase == Phase.End)
+        {
+            return;
+        }
+
+        playerPos = room.PlayersInRoom.First().mainBodyChunk.pos;
+        Debug.Log("Current player position: " + playerPos);
         var game = room.game;
 
-        if (currentPhase == Phase.Init)
+        if (Vector2.Distance(playerPos, craftTriggerPos) <= triggerReach && nextPhase == Phase.Craft)
         {
             game.cameras.First().hud.textPrompt.AddMessage(
-                game.manager.rainWorld.inGameTranslator.Translate("Press (GRAFFITI MODE + CRAFT) while carrying a rock and colorful object to craft a Spray Can."),
+                game.manager.rainWorld.inGameTranslator.Translate("Hold (GRAFFITI MODE + CRAFT) while carrying a rock and colorful object to craft a Spray Can."),
                 0, 600, false, false
             );
             game.cameras.First().hud.textPrompt.AddMessage(
-                game.manager.rainWorld.inGameTranslator.Translate("Different objects produce different levels of Spray Cans. You can combine two Spray Cans, or upgrade one with another object."),
-                800, 600, false, false
+                game.manager.rainWorld.inGameTranslator.Translate("Different objects produce different levels of Spray Cans. You can also combine two Spray Cans, or upgrade one with another object."),
+                100, 600, false, false
             );
-            currentPhase = Phase.Craft;
+            nextPhase = Phase.Spray;
+        }
+        else if (Vector2.Distance(playerPos, sprayTriggerPos) <= triggerReach && nextPhase == Phase.Spray)
+        {
+            game.cameras.First().hud.textPrompt.AddMessage(
+                game.manager.rainWorld.inGameTranslator.Translate("Press (GRAFFITI MODE + SPRAY) while carrying a Spray Can to spend a charge and spray graffiti."),
+                0, 600, false, false
+            );
+            nextPhase = Phase.Throw;
+        }
+        else if (Vector2.Distance(playerPos, throwTriggerPos) <= triggerReach && nextPhase == Phase.Throw)
+        {
+            game.cameras.First().hud.textPrompt.AddMessage(
+                game.manager.rainWorld.inGameTranslator.Translate("Throwing a Spray Can will create a large, non-lethal explosion of compressed paint."),
+                0, 600, false, false
+            );
+            game.cameras.First().hud.textPrompt.AddMessage(
+                game.manager.rainWorld.inGameTranslator.Translate("The more charages a Spray Can has, the more force the explosion will have. (The max charges of a Spray Can is 3)"),
+                100, 600, false, false
+            );
+            nextPhase = Phase.End;
         }
     }
 }
