4cabb25fc284e48577320225ef75783060b1fd17
Cole Strain <colestrain2000@gmail.com>
2024-04-11 12:34:56 -0700

    Move robut and dead scav

--- src/Scripts/CutsceneVinkiRobo.cs
+++ src/Scripts/CutsceneVinkiRobo.cs
@@ -12,6 +12,11 @@ namespace Vinki
             phase = Phase.Init;
             bot = new AncientBot(roboPos, new Color(0.945f, 0.3765f, 0f), null, false);
             room.AddObject(bot);
+
+            // Dead scav (lol)
+            AbstractCreature abstractCreature = new AbstractCreature(room.world, StaticWorld.GetCreatureTemplate(MoreSlugcatsEnums.CreatureTemplateType.ScavengerElite), null, new WorldCoordinate(room.abstractRoom.index, scavX, scavY, -1), room.game.GetNewID());
+            abstractCreature.state.Die();
+            room.abstractRoom.AddEntity(abstractCreature);
         }
 
         public override void Update(bool eu)
@@ -73,7 +78,8 @@ namespace Vinki
         public AncientBot bot;
         public bool initController;
         public Phase phase;
-        public Vector2 roboPos = new(520f, 640f);
+        public Vector2 roboPos = new(570f, 960f);
+        public int scavX = 30, scavY = 47;
         public float activateRadius = 100f;
         public class Phase(string value, bool register = false) : ExtEnum<Phase>(value, register)
         {
