f0c9340222f2803bb0caa5eab7836c2593961655
Cole Strain <colestrain2000@gmail.com>
2024-06-13 00:21:47 -0700

    SlugcatSelectMenu.StartGame IL Hook works!

--- src/Hooks/Menu/SlugcatSelectMenu.cs
+++ src/Hooks/Menu/SlugcatSelectMenu.cs
@@ -10,8 +10,15 @@ public static partial class Hooks
     private static void ApplySlugcatSelectMenuHooks()
     {
         On.Menu.SlugcatSelectMenu.CommunicateWithUpcomingProcess += SlugcatSelectMenu_CommunicateWithUpcomingProcess;
-        IL.Menu.SlugcatSelectMenu.StartGame -= SlugcatSelectMenu_StartGame;
-        IL.Menu.SlugcatSelectMenu.StartGame += SlugcatSelectMenu_StartGame;
+
+        try
+        {
+            IL.Menu.SlugcatSelectMenu.StartGame += SlugcatSelectMenu_StartGame;
+        }
+        catch (Exception ex)
+        {
+            Plugin.VLogger.LogError("Could not apply StartGame IL hook\n" + ex.Message);
+        }
     }
 
     private static void SlugcatSelectMenu_CommunicateWithUpcomingProcess(On.Menu.SlugcatSelectMenu.orig_CommunicateWithUpcomingProcess orig, SlugcatSelectMenu self, MainLoopProcess nextProcess)
@@ -26,34 +33,35 @@ public static partial class Hooks
 
     private static void SlugcatSelectMenu_StartGame(ILContext il)
     {
-        Plugin.VLogger.LogInfo("Applying slugcat select hook");
         var c = new ILCursor(il);
         ILLabel soundLabel = null;
 
-        c.GotoNext(
-            x => x.MatchLdarg(0),
-            x => x.MatchLdfld<MainLoopProcess>("manager"),
-            x => x.MatchLdsfld<ProcessManager.ProcessID>("Game"),
-            x => x.MatchCallvirt<ProcessManager>("RequestMainProcessSwitch")
-        );
-        Plugin.VLogger.LogInfo("Moved cursor to before Game process switch");
-        new ILCursor(c).GotoNext(x => x.MatchBr(out soundLabel));
-        Plugin.VLogger.LogInfo("Moved new cursor to " + soundLabel.Target.OpCode.Name.ToString());
-        c.Emit(OpCodes.Ldarg_0);
-        c.Emit(OpCodes.Ldarg_1);
-        c.EmitDelegate((SlugcatSelectMenu self, SlugcatStats.Name storyGameCharacter) =>
+        try
         {
-            Plugin.VLogger.LogInfo("In delegate; " + storyGameCharacter.value);
-            if (storyGameCharacter == Enums.vinki)
-            {
-                Plugin.VLogger.LogInfo("We are Vinki");
-                self.manager.RequestMainProcessSwitch(Enums.FullscreenVideo);
-                return true;
-            }
-            return false;
-        });
-        c.Emit(OpCodes.Brtrue_S, soundLabel);
+            c.GotoNext(
+                x => x.MatchLdarg(1),
+                x => x.MatchLdsfld<SlugcatStats.Name>(nameof(SlugcatStats.Name.White)),
+                x => x.MatchCall(typeof(ExtEnum<SlugcatStats.Name>).GetMethod("op_Inequality"))
+            );
 
-        Plugin.VLogger.LogDebug(il.ToString());
+            new ILCursor(c).GotoNext(x => x.MatchBr(out soundLabel));
+
+            c.Emit(OpCodes.Ldarg_0);
+            c.Emit(OpCodes.Ldarg_1);
+            c.EmitDelegate((SlugcatSelectMenu self, SlugcatStats.Name storyGameCharacter) =>
+            {
+                if (storyGameCharacter == Enums.vinki)
+                {
+                    self.manager.RequestMainProcessSwitch(Enums.FullscreenVideo);
+                    return true;
+                }
+                return false;
+            });
+            c.Emit(OpCodes.Brtrue_S, soundLabel);
+        }
+        catch (Exception e) 
+        { 
+            Plugin.VLogger.LogError("Could not complete StartGame IL Hook\n" + e.Message); 
+        }
     }
 }
