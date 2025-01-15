748829ead82df86cca619f4720633b4144393c3a
Cole Strain <colestrain2000@gmail.com>
2025-01-15 10:33:16 -0800

    ComboDisplay fades out after combo ends

--- src/Scripts/ComboDisplay.cs
+++ src/Scripts/ComboDisplay.cs
@@ -9,6 +9,7 @@ public class ComboDisplay : HudPart
 {
     public Vector2 pos;
     public Vector2 lastPos;
+    public int opacityTimer = 0;
 
 	public VinkiPlayerData vinki;
 	public FLabel currentTrick;
@@ -74,19 +75,43 @@ public class ComboDisplay : HudPart
     public override void Update()
     {
         this.lastPos = this.pos;
+
+        if (vinki.timeLeftInCombo <= 0)
+        {
+            this.opacityTimer--;
+        }
+        else
+        {
+            this.opacityTimer = 200;
+        }
     }
 
     public override void Draw(float timeStacker)
     {
         this.currentTrick.x = this.DrawPos(timeStacker).x - 25f;
-        this.currentTrick.text = vinki.currentTrickName;
         this.currentCombo.x = this.DrawPos(timeStacker).x + 120f;
-        this.currentCombo.text = "x" + vinki.comboSize.ToString();
         this.trickScore.x = this.DrawPos(timeStacker).x + 90f;
-        this.trickScore.text = vinki.currentTrickScore.ToString();
         this.comboTimer.scaleX = Mathf.Lerp(0f, 15f, vinki.timeLeftInCombo / 400f);
         this.totalScore.x = this.DrawPos(timeStacker).x + 120f;
-        this.totalScore.text = vinki.comboTotalScore.ToString();
+
+        // Don't update the text if the combo is over, so players have a chance to see their end score and combo
+        if (vinki.timeLeftInCombo > 0)
+        {
+            this.currentTrick.text = vinki.currentTrickName;
+            this.currentCombo.text = "x" + vinki.comboSize.ToString();
+            this.trickScore.text = vinki.currentTrickScore.ToString();
+            this.totalScore.text = vinki.comboTotalScore.ToString();
+        }
+
+        float alpha = 1f;
+        if (this.opacityTimer <= 120)
+        {
+            alpha = Mathf.Lerp(0f, 1f, this.opacityTimer / 120f);
+        }
+        this.currentTrick.alpha = alpha;
+        this.currentCombo.alpha = alpha;
+        this.trickScore.alpha = alpha;
+        this.totalScore.alpha = alpha;
     }
 
     public Vector2 DrawPos(float timeStacker)
