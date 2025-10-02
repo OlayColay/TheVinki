8f11c42260bbd23ded28094573b683c88136afc0
Cole Strain <colestrain2000@gmail.com>
2025-10-01 23:04:12 -0700

    Remove WiP stuff

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -15,7 +15,7 @@ namespace Vinki
     [BepInDependency("dressmyslugcat", BepInDependency.DependencyFlags.SoftDependency)]
     [BepInDependency("improved-input-config", BepInDependency.DependencyFlags.SoftDependency)]
     [BepInDependency("pushtomeow", BepInDependency.DependencyFlags.SoftDependency)]
-    [BepInPlugin(MOD_ID, "The Vinki", "0.13.1.4")]
+    [BepInPlugin(MOD_ID, "The Vinki", "0.13.1.5")]
     class Plugin : BaseUnityPlugin
     {
         public const string MOD_ID = "olaycolay.thevinki";
